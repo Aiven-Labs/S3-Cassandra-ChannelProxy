d6d6d84189994ed3145e95c0c8efb33f86dc44c4
Claude Warren <claude.warren@aiven.io>
2023-09-26 15:45:08 +0200

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # S3-Cassandra-ChannelProxy
-An Cassandra ChannelProxyFactory to read/write AWS S3 storage
+A Cassandra ChannelProxyFactory to read/write AWS S3 storage
+
+## Building 
+To build this project you will need the jar from building the Cassandra code with CEP-36 [^1]  and you need to build the included submodule `s3fs-nio`.  The Cassandra lib and source lib should be placed in the `lib` subdirectory.  You may need to add 'lib' as a library.
+
+### Building `s3fs-nio`
+
+```
+cd s3fs-nio
+./gradlew
+./publishToMavenLocal
+```
+
+
+[^1]: https://github.com/claudenw/cassandra/tree/channel_proxy_factory
+
---
+++ src/main/java/oy/aiven/cassandra/S3ChannelProxyFactory.java
@@ -0,0 +1,74 @@
+package oy.aiven.cassandra;
+
+import static org.carlspring.cloud.storage.s3fs.S3Factory.ACCESS_KEY;
+import static org.carlspring.cloud.storage.s3fs.S3Factory.SECRET_KEY;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.cassandra.io.util.ChannelProxy;
+import org.apache.cassandra.io.util.ChannelProxyFactory;
+import org.apache.cassandra.io.util.File;
+
+public class S3ChannelProxyFactory extends ChannelProxyFactory {
+
+    private final FileSystem fileSystem;
+    private final String source;
+
+    private static Map<String, ?> propertyMap() {
+        Map<String, Object> result = new HashMap<>();
+        result.put(ACCESS_KEY, "access key");
+        result.put(SECRET_KEY, "secret key");
+        return Collections.unmodifiableMap(result);
+    }
+
+    protected S3ChannelProxyFactory(Map<String, String> args) throws IOException {
+        super(null, null);
+        this.readerFactory = this::readerProxy;
+        this.writerFactory = this::writerProxy;
+
+        fileSystem = FileSystems.newFileSystem(URI.create("s3:///"), propertyMap(),
+                Thread.currentThread().getContextClassLoader());
+        source = args.get("source");
+    }
+
+    private ChannelProxy readerProxy(File file) {
+        if (file.path().startsWith(source)) {
+            Path path = fileSystem.getPath(file.path().substring(source.length()));
+            try {
+                return new ChannelProxy(file, FileChannel.open(path, StandardOpenOption.READ));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return new ChannelProxy(file);
+    }
+
+    private ChannelProxy writerProxy(File file) {
+        if (file.path().startsWith(source)) {
+
+            ChannelProxy result = null;
+            Path path = fileSystem.getPath(file.path().substring(source.length()));
+            try {
+                if (path.toFile().exists()) {
+                    result = new ChannelProxy(file, FileChannel.open(path, StandardOpenOption.WRITE));
+                } else {
+                    result = new ChannelProxy(file,
+                            FileChannel.open(path, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return result;
+        }
+        return new ChannelProxy(file);
+    }
+}
